b1cb458c8c5db0c3b470e9118a8a66bde45cdb45
Narendhiran17 <151978622+Narendhiran17@users.noreply.github.com>
2026-07-24 15:04:36 +0530

    Delete api directory

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-# Ensure the root directory is in the python path to resolve 'app.*' imports on Vercel
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app.main import app
